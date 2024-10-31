f1bcb2ce0b5e65b86ae68d9b95f225112bf07b1c
nikos <nikgiannsm@gmail.com>
2024-10-31 15:25:15 +0200

    Added connection to db

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
+import os #.env reader
+import psycopg2 #postgresql library
+from dotenv import load_dotenv #environment variables loader
 from flask import Flask
 
+load_dotenv()
+
 app = Flask(__name__)
+
+host = os.getenv("HOST")
+port = os.getenv("POSTGRESQL_PORT")
+database = os.getenv("DATABASE")
+user = os.getenv("POSTGRESQL_USER")
+password = os.getenv("PASSWORD")
+
+try:
+    connection = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password
+    )
+    print("Database connection successful!")
+except psycopg2.Error as e:
+    print(f"Error connecting to the database: {e}")
+    connection = None
 
 @app.get("/")
 def home():
